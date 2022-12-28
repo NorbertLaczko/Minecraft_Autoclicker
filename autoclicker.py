ebadffe7fdf70e7f5122df1c836ff2ec8b906db5
NorbertLaczko <121566857+NorbertLaczko@users.noreply.github.com>
2022-12-28 16:43:22 +0100

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,90 @@
+import pynput.mouse as mouse
+from pynput.mouse import Button
+import pynput.keyboard as keyboard
+from pynput.keyboard import Key
+import logging, coloredlogs
+import pyfiglet
+from sys import exit
+import time
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s',)
+
+coloredlogs.install(level=('DEBUG'), fmt='%(message)s')
+
+class autoclicker:
+
+    key = None
+
+    start = Key.f9
+    pause = Key.f10
+    stop = Key.f11
+
+    if start == key:
+        logging.info('Autoclicker: On')
+    elif pause == key:
+        logging.info('Autoclicker: Off')
+
+
+    def menu(self):
+        figlet = pyfiglet.figlet_format('AUTOCLICKER')
+        logging.warning(f'{figlet}')
+        logging.debug('Start program:f9')
+        logging.debug('Pause program:f10')
+        logging.debug('Close Program:f11')
+
+
+    def on_press(self, key):
+        self.key = key
+
+    def pause_1(self):
+            pass
+
+    def start_listening(self):
+        listener = keyboard.Listener(on_press=self.on_press)
+        listener.start()
+
+        self.menu()
+
+        msg_db = 0
+
+        while True:
+
+            while True:
+
+                if self.key == self.stop:
+                    exit()
+
+                elif self.key == self.pause:
+                    while not self.key == self.start and not self.key == self.stop:
+                        time.sleep(0.01)
+                    
+                    if self.key == self.start:
+                        logging.error('Autoclier: ON')
+
+                elif self.key == self.start:
+                    while not self.key == self.pause and not self.key == self.stop:
+                        controll = mouse.Controller()
+                        time.sleep(0.001)
+                        controll.click(button=Button.left)
+                    if self.key == self.pause:
+                        logging.error('Autoclicker: OFF')
+
+
+
+click = autoclicker()
+click.start_listening()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
